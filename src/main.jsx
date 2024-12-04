af4cb4521458a3c98db3c6344ecbd1287c999fbc
John Doe <johndoe@email.com>
2024-12-04 14:17:04 +0800

    fix to hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import FacebookPage from "./pages/facebook/Facebook.jsx";
 import RobloxPage from "./pages/roblox/Roblox.jsx";
 import App from "./pages/App.jsx";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/facebook", element: <FacebookPage /> },
-  { path: "/roblox", element: <RobloxPage /> },
-]);
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/facebook" element={<FacebookPage />} />
+        <Route path="/roblox" element={<RobloxPage />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
